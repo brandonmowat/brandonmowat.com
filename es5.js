8166a6e309d81cc677181c5e5d84749d4728148a
Brandon <brandonmowat11@gmail.com>
2016-09-08 13:51:13 -0400

    Updated styles

--- es5.js
+++ es5.js
@@ -10,6 +10,10 @@ var _reactDom = require('react-dom');
 
 var _reactChatUi = require('react-chat-ui');
 
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -65,6 +69,8 @@ var Chat = function (_React$Component) {
               fontSize: 18
             },
             chatbubble: {
+              borderRadius: 100,
+              marginLeft: 0,
               maxWidth: 600
             },
             userBubble: {
@@ -83,5 +89,15 @@ var Chat = function (_React$Component) {
 
   return Chat;
 }(_react2.default.Component);
+
+function rotate() {
+  (0, _jquery2.default)("#logo").hover(function () {
+    (0, _jquery2.default)("#logo").addClass('rotate');
+  }, function () {
+    (0, _jquery2.default)("#logo").removeClass('rotate');
+  });
+}
+
+rotate();
 
 (0, _reactDom.render)(_react2.default.createElement(Chat, null), document.getElementById('chat-section'));
---
+++ index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { ChatFeed } from 'react-chat-ui';
+import $ from 'jquery';
+
+class Chat extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      messages : [
+        {type:1, message: "I can help with lots of stuff."}, // Gray bubble
+      ],
+      is_typing: false
+    }
+  }
+
+  _pushMessage(recipient, message) {
+    var prevState = this.state
+    prevState.messages.push({type: recipient, message: message})
+    this.setState(this.state)
+  }
+
+  _onMessageSubmit(e) {
+    var input = this.refs.message;
+    e.preventDefault();
+    if (!input.value) {return false;}
+    this._pushMessage(0, input.value)
+    input.value = '';
+
+  }
+
+  render() {
+    return(
+      <div>
+        <ChatFeed
+          messages={this.state.messages} // Boolean: list of message objects
+          is_typing={this.state.is_typing} // Boolean: is the recipient typing
+          bubbleStyles={{ // JSON: Custom bubble styles
+            text: {
+              fontSize: 18,
+            },
+            chatbubble: {
+              borderRadius: 100,
+              marginLeft: 0,
+              maxWidth: 600,
+            },
+            userBubble: {
+              backgroundColor: '#0084FF',
+            }
+          }}
+        />
+
+      <form onSubmit={this._onMessageSubmit.bind(this)}>
+        <input ref="message" placeholder="Type a message..." className="message-input" />
+      </form>
+      </div>
+    )
+  }
+}
+
+function rotate() {
+    $("#logo").hover(
+      () => {
+        $("#logo").addClass('rotate');
+      },
+      () => {
+        $("#logo").removeClass('rotate');
+      }
+    );
+}
+
+rotate();
+
+render(
+  (<Chat/>)
+  ,
+  document.getElementById('chat-section')
+)
